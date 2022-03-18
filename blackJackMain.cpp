736bdd29f10cfafca16b2dd8b16004eecbc85a91
Adrian <77588376+Pr1ncipal@users.noreply.github.com>
2022-03-18 10:22:24 -0400

    minor changes
    added comments, uniformity, and called the start function.

--- blackJackMain.cpp
+++ blackJackMain.cpp
@@ -14,12 +14,18 @@ int main()
 
     srand((unsigned)time(0)); //dont really know but lets u choose a random number. we used it in a previous lab
 
+    /*srand is how you make a random number generator. however, since a computer doesn't understand what "random" actually is, you have to tell the computer where
+    you want it to find the number from. in windows/c++/something, there is a way it finds the time in the background (ctime), but the time isn't in human
+    readable date form, its just a bunch of numbers. In essence, this is a simple way to generate a "random number". while the number isn't TRULY random and could
+    technically be found and used to cheat, that would be excessively diffifcult just to gimp a RNG. - Adrian
+    */
+
     //class called blackType
 
     int balance = 100, wager;
 
     //uses the start function
-
+    start();
 
     //calls the bet function
     bet(wager);
@@ -30,7 +36,9 @@ int main()
 void start()
 {
      //add rules and directions maybe
-    cout<<"Welcome to the sus game of Black Jack. You start with 100 chips."<<endl;
+    cout << "\nWelcome to the sus game of Black Jack. Since this is a new game, you start with 100 chips.\n" << endl;
+
+    cout << "Rules & directions go here....\n" << endl;
 }
 
 
@@ -40,13 +48,13 @@ void bet(int wager)
     cout<<"input the amount of chips you woul like to wager. minimum wager is 1 chip."<<endl;
     cin>>wager;
    
-    if(wager>100 || wager<1)
+    if(wager > 100 || wager < 1)
     {
-        cout<<"your bet is incorrect. input again please.";
-        cin>>wager;
+        cout << "your bet is incorrect. input again please.";
+        cin >> wager; 
     }
     else
     {
-        cout<<"you bet "<<wager;
+        cout << "you bet " << wager;
     }
 }
