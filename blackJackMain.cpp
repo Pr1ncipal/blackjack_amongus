14fa4f927ab2e3ea72863ee5d398fae98503837b
BakedZito <101850483+BakedZito@users.noreply.github.com>
2022-03-19 00:21:37 -0400

    added while loop, win function, cleaned things up
    ace needs fixing
    outputting how much you win needs fixing

--- blackJackMain.cpp
+++ blackJackMain.cpp
@@ -7,9 +7,11 @@
 
 using namespace std;
 //initialize the bet function
-void bet(int wager);
+void bet(int wager, int& balance);
 void start();
-void dealer();
+void dealer(int& wager);
+
+int balance = 100;
 
 int main()
 {
@@ -24,114 +26,21 @@ int main()
 
     //class called blackType
 
-    int balance = 100, wager;
+    int wager;
 
-    //Asks the player if they need a tutorial or if they want to start the game.
-    bool x = 1;
-    cout << "Do you need a tutorial? 1 for yes, 0 for no." << endl;
-    cin >> x;
+    //uses the start function
+    start();
 
-    switch (x)
+    while(balance > 0)
     {
-        case 1:
-            //tutorial();
-            break;
-        case 0:
-            start();
-            break;
-    }
-
     //calls the bet function
-    bet(wager);
-  
-    dealer();
-    
+    bet(wager, balance);
+    dealer(wager);
+
+{
+    }
     return 0;
 }
-
-void cardPrinter()
-{
-int x;
-switch (x)
-{
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    {
-        cout << "**********" << endl;
-        cout << "*      " << x << " *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "* " << x << "      *" << endl;
-        cout << "**********" << endl;
-        break;
-    }
-    case 10:
-    {
-        cout << "**********" << endl;
-        cout << "*      " << x << "*" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "*" << x << "      *" << endl;
-        cout << "**********" << endl;
-        break;
-    }
-    case 11:
-    {
-        cout << "**********" << endl;
-        cout << "*      " << "J" << " *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "* " << "J" << "      *" << endl;
-        cout << "**********" << endl;
-        break;
-    }
-    case 12:
-    {
-        cout << "**********" << endl;
-        cout << "*      " << "Q" << " *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "* " << "Q" << "      *" << endl;
-        cout << "**********" << endl;
-        break;
-    }
-    case 13:
-    {
-        cout << "**********" << endl;
-        cout << "*      " << "K" << " *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "* " << "K" << "      *" << endl;
-        cout << "**********" << endl;
-        break;
-    }
-    case 1:
-    case 14:
-    {
-        cout << "**********" << endl;
-        cout << "*      " << "A" << " *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "*        *" << endl;
-        cout << "* " << "A" << "      *" << endl;
-        cout << "**********" << endl;
-        break;
-    }
-
-} //Ends switch statement
-    
-} // Ends function.
 
 void start()
 {
@@ -141,29 +50,14 @@ void start()
     cout << "Rules & directions go here....\n" << endl;
 }
 
-void tutorial()
-{
-    cout << "When you play the house, you play against the casino, which is represented by the dealer. " << endl;
-    cout << "The dealer deals one card face up to each player, from left to right, with the last card going to the houses hand, which is face down. " << endl;
-    cout << "The dealer will then deal one card facing up to each player and then the house." << endl;
-    
-    cout << "After the initial deal, the blackjack rules indicate that the dealer will ask each player, in succession, if he/she needs one or more cards." << endl; 
-    cout << "As the player, you can ask for one or more cards(called a hit)until you either go over 21 (bust), or you think you have the best possible hand." << endl;
 
-    cout << "Once you have all the cards you need, you stay or stand, meaning you signal to the dealer that you dont want any more cards. " << endl;
-    cout << "Each subsequent player then decides whether to hit or stand." << endl;
-
-    cout << "After all the players have completed their hands or gone bust, the dealer reveals his or her down card. " << endl;
-    cout << "Depending on the cards in the dealers hand, the blackjack rules at the table will dictate whether the dealer will hit or stand." << endl;
-}
-
-//betting functions
-void bet(int wager)
+//betting functions20
+void bet(int wager, int& balance)
 {
     cout<<"input the amount of chips you woul like to wager. minimum wager is 1 chip."<<endl;
     cin>>wager;
    
-    if(wager > 100 || wager < 1)
+    if(wager > 100 || wager < 1 || wager > balance)
     {
         cout << "your bet is incorrect. input again please.";
         cin >> wager; 
@@ -171,15 +65,18 @@ void bet(int wager)
     else
     {
         cout << "you bet " << wager <<" chips. "<<endl;
+        balance = balance - wager;
+        cout<<"your current balance is "<<balance<<endl;
     }
 }
 
-void dealer()
+void dealer(int& wager) //ace needs fixing
 {
     int dCard1 = rand() % 13 + 1;
     int dCard2 = rand() % 13 + 1;
     int pCard1 = rand() % 13 + 1;
     int pCard2 = rand() % 13 + 1;
+    int totalD, totalP;
 
     cout<<"the dealer has "<<endl;
 
@@ -240,6 +137,10 @@ void dealer()
 			break;
         }
     }
+    cout<<endl;
+    totalD = dCard1 + dCard2;
+    cout<<"dealer's total is "<<dCard1 + dCard2<<endl;
+        //needs to be fixed for aces being 11 and 1
 //--------------------------------------------------------------------------------------
     cout<<endl;
     cout<<endl;
@@ -301,4 +202,26 @@ void dealer()
 			break;
         }
     }
+    cout<<endl;
+    totalP = pCard1 + pCard2;
+    int winning = 1.5*wager; //for some reason wager only outputs $0
+    cout<<"the player's total is "<< totalP<<endl;
+
+//temporary should move to another func
+    if(pCard1 == 1 && pCard2 == 10|11|12|13)
+    {
+        cout<<"Black Jack! You win "<<winning<<"!"<<endl;
+    } 
+    else if(pCard2 == 1 && pCard1 == 10|11|12|13)
+    {
+        cout<<"Black Jack! You win "<<winning<<"!"<<endl;
+    }
+    else if(totalP>totalD && totalP<21)
+    {
+        cout<<"you win! $"<<wager*wager<<"!"<<endl;
+    }
+    else
+        cout<<"you lose :( $"<<wager<<endl;
+        
+    //needs to be fixed for aces being 11 and 1
 }
